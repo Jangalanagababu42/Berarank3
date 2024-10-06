426183fb3ce3a8434789a40443cf8b7a0aebf0b2
Jangalanagababu42 <jangalanagababu42@gmail.com>
2024-10-06 13:41:06 +0530

    fix:worked on comments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,130 @@
+// import { useState } from 'react';
+// import lovdlogo from '../assets/lovdlogo.svg';
+// import magnify from '../assets/magnify.svg';
+// import logo_berarank from "../assets/logo_berarank.svg";
+// const Header = () => {
+ 
+//   return (
+//     <header className="sticky top-0  z-50 w-full px-4 px-[160px] py-4 bg-white ">
+//       <div className="grid grid-cols-12 items-center ">
+//         {/* Logo Div */}
+//         <div className="col-start-1 col-span-3 flex flex-col items-center space-x-3  ">
+//           <div className='flex space-x-2 items-center'>
+
+//           <img
+//             src={logo_berarank}
+//             alt="L0vd Logo"
+//             className="w-[50px] h-[42px] "
+//           />
+//              <span className="font-poppins font-semibold text-3xl tracking-wider">
+//                Berarank
+//              </span>
+//           </div>
+//            <div className="flex ">
+//              <span className="font-poppins font-medium text-sm text-grey-table space-x-1">
+//                powered by <img src={lovdlogo} alt="Powered by L0vd" className="inline-block w-4 h-4 ml-1" />
+//                <span className='font-semibold text-black'>
+
+//                L0vd
+//                </span>
+//            </span>
+//           </div>
+//          </div>
+
+//         {/* Search Bar */}
+//         <div className="col-start-4 col-span-3 flex items-center w-[250px] justify-center  ">
+//           <div className="relative w-full max-w-md  ">
+//             <input
+//               type="text"
+//               placeholder="Search"
+//               className="w-full h-[45px] ring-1 ring-inset ring-lightblue-stroke focus:outline-none focus:ring-1 focus:ring-inset focus:ring-lightblue-stroke  font-poppins font-medium text-[16px] text-grey-stroke rounded-[10px]  px-12 py-3 md:py-4"
+//             />
+//             <span className="absolute top-1/2 left-4 transform -translate-y-1/2 text-gray-400  ">
+//               <img src={magnify} alt="magnify" className="w-6 h-6 " />
+//             </span>
+//           </div>
+//         </div>
+
+//         {/* Right Section: Validators, Pools, Guides, and Connect Button */}
+//         <div className="col-start-7 col-span-5  flex  justify-between items-center  h-[53px] space-x-12">
+//           <div className="flex w-[279px] h-[44px] space-x-[30px] items-center font-poppins font-medium text-base">
+//             <a
+//               href="#"
+//               className="text-black px-[10px] py-[10px] hover:bg-gradient-to-br from-grad-blue from-0% to-main-blue to-100% hover:text-white hover:px-[10px] hover:py-[5px] hover:rounded-[10px] hover:ring-1 hover:ring-offset hover:ring-main-blue"
+//             >
+//               Validators
+//             </a>
+//             <a
+//               href="#"
+//               className="text-black px-[10px] py-[10px] hover:bg-gradient-to-br from-grad-blue from-0% to-main-blue to-100% hover:text-white hover:px-[10px] hover:py-[5px] hover:rounded-[10px] hover:ring-1 hover:ring-offset hover:ring-main-blue"
+             
+//             >
+             
+//              Pools
+//             </a>
+//             <a
+//               href="#"
+//               className="text-black px-[10px] py-[10px] hover:bg-gradient-to-br from-grad-blue from-0% to-main-blue to-100% hover:text-white hover:px-[10px] hover:py-[5px] hover:rounded-[10px] hover:ring-1 hover:ring-offset hover:ring-main-blue"
+            
+//             >
+//                Guides
+//             </a>
+//           </div>
+//           <button className=" w-[187px] h-[53px] bg-main-blue text-white px-5 py-2.5  rounded-[10px] font-poppins font-medium text-white text-md">
+//             Connect wallet
+//           </button>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
 import { useState } from 'react';
 import lovdlogo from '../assets/lovdlogo.svg';
 import magnify from '../assets/magnify.svg';
 import logo_berarank from "../assets/logo_berarank.svg";
+
 const Header = () => {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [showTooltip, setShowTooltip] = useState({ pools: false, guides: false });
 
-  const handleMouseEnter = (index) => {
-    setHoveredIndex(index);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredIndex(null);
-  };
   return (
-    <header className="sticky top-0  z-50 w-full px-4 px-[160px] py-4 bg-white ">
-      <div className="grid grid-cols-12 items-center ">
+    <header className="sticky top-0 z-50 w-full px-4 px-[160px] py-4 bg-white">
+      <div className="grid grid-cols-12 items-center">
         {/* Logo Div */}
-        <div className="col-start-1 col-span-3 flex flex-col items-center space-x-3  ">
+        <div className="col-start-1 col-span-3 flex flex-col items-center space-x-3">
           <div className='flex space-x-2 items-center'>
-
-          <img
-            src={logo_berarank}
-            alt="L0vd Logo"
-            className="w-[50px] h-[42px] "
-          />
-             <span className="font-poppins font-semibold text-3xl tracking-wider">
-               Berarank
-             </span>
+            <img src={logo_berarank} alt="L0vd Logo" className="w-[50px] h-[42px]" />
+            <span className="font-poppins font-semibold text-3xl tracking-wider">
+              Berarank
+            </span>
           </div>
-           <div className="flex ">
-             <span className="font-poppins font-medium text-sm text-grey-table space-x-1">
-               powered by <img src={lovdlogo} alt="Powered by L0vd" className="inline-block w-4 h-4 ml-1" />
-               <span className='font-semibold text-black'>
-
-               L0vd
-               </span>
-           </span>
+          <div className="flex">
+            <span className="font-poppins font-medium text-sm text-grey-table space-x-1">
+              powered by <img src={lovdlogo} alt="Powered by L0vd" className="inline-block w-4 h-4 ml-1" />
+              <span className='font-semibold text-black'>
+                L0vd
+              </span>
+            </span>
           </div>
-         </div>
+        </div>
 
         {/* Search Bar */}
-        <div className="col-start-4 col-span-3 flex items-center w-[250px] justify-center  ">
-          <div className="relative w-full max-w-md  ">
+        <div className="col-start-4 col-span-3 flex items-center w-[250px] justify-center">
+          <div className="relative w-full max-w-md">
             <input
               type="text"
               placeholder="Search"
-              className="w-full h-[45px] ring-1 ring-inset ring-lightblue-stroke focus:outline-none focus:ring-1 focus:ring-inset focus:ring-lightblue-stroke  font-poppins font-medium text-[16px] text-grey-stroke rounded-[10px]  px-12 py-3 md:py-4"
+              className="w-full h-[45px] ring-1 ring-inset ring-lightblue-stroke focus:outline-none focus:ring-1 focus:ring-inset focus:ring-lightblue-stroke font-poppins font-medium text-[16px] text-grey-stroke rounded-[10px] px-12 py-3 md:py-4"
             />
-            <span className="absolute top-1/2 left-4 transform -translate-y-1/2 text-gray-400  ">
-              <img src={magnify} alt="magnify" className="w-6 h-6 " />
+            <span className="absolute top-1/2 left-4 transform -translate-y-1/2 text-gray-400">
+              <img src={magnify} alt="magnify" className="w-6 h-6" />
             </span>
           </div>
         </div>
 
         {/* Right Section: Validators, Pools, Guides, and Connect Button */}
-        <div className="col-start-7 col-span-5  flex  justify-between items-center  h-[53px] space-x-12">
+        <div className="col-start-7 col-span-5 flex justify-between items-center h-[53px] space-x-12">
           <div className="flex w-[279px] h-[44px] space-x-[30px] items-center font-poppins font-medium text-base">
             <a
               href="#"
@@ -62,25 +132,42 @@ const Header = () => {
             >
               Validators
             </a>
-            <a
-              href="#"
-              className="text-black px-[10px] py-[10px] hover:bg-gradient-to-br from-grad-blue from-0% to-main-blue to-100% hover:text-white hover:px-[10px] hover:py-[5px] hover:rounded-[10px] hover:ring-1 hover:ring-offset hover:ring-main-blue"
-              onMouseEnter={() => handleMouseEnter(1)}
-              onMouseLeave={handleMouseLeave}
+            <div
+              className="relative"
+              onMouseEnter={() => setShowTooltip({ ...showTooltip, pools: true })}
+              onMouseLeave={() => setShowTooltip({ ...showTooltip, pools: false })}
             >
-             
-             {hoveredIndex === 1 ? <span className='text-sm'>coming soon</span> : 'Pools'}
-            </a>
-            <a
-              href="#"
-              className="text-black px-[10px] py-[10px] hover:bg-gradient-to-br from-grad-blue from-0% to-main-blue to-100% hover:text-white hover:px-[10px] hover:py-[5px] hover:rounded-[10px] hover:ring-1 hover:ring-offset hover:ring-main-blue"
-              onMouseEnter={() => handleMouseEnter(2)}
-              onMouseLeave={handleMouseLeave}
+              <a
+                href="#"
+                className="text-black px-[10px] py-[10px] hover:bg-gradient-to-br from-grad-blue from-0% to-main-blue to-100% hover:text-white hover:px-[10px] hover:py-[5px] hover:rounded-[10px] hover:ring-1 hover:ring-offset hover:ring-main-blue"
+              >
+                Pools
+              </a>
+              {showTooltip.pools && (
+                <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 p-2 w-max bg-black text-white text-sm rounded-lg shadow-lg">
+                  Coming Soon
+                </div>
+              )}
+            </div>
+            <div
+              className="relative"
+              onMouseEnter={() => setShowTooltip({ ...showTooltip, guides: true })}
+              onMouseLeave={() => setShowTooltip({ ...showTooltip, guides: false })}
             >
-               {hoveredIndex === 2 ? <span className='text-sm'>coming soon</span> : 'Guides'}
-            </a>
+              <a
+                href="#"
+                className="text-black px-[10px] py-[10px] hover:bg-gradient-to-br from-grad-blue from-0% to-main-blue to-100% hover:text-white hover:px-[10px] hover:py-[5px] hover:rounded-[10px] hover:ring-1 hover:ring-offset hover:ring-main-blue"
+              >
+                Guides
+              </a>
+              {showTooltip.guides && (
+                <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 p-2 w-max bg-black text-white text-sm rounded-lg shadow-lg">
+                  Coming Soon
+                </div>
+              )}
+            </div>
           </div>
-          <button className=" w-[187px] h-[53px] bg-main-blue text-white px-5 py-2.5  rounded-[10px] font-poppins font-medium text-white text-md">
+          <button className="w-[187px] h-[53px] bg-main-blue text-white px-5 py-2.5 rounded-[10px] font-poppins font-medium text-white text-md">
             Connect wallet
           </button>
         </div>
@@ -90,73 +177,3 @@ const Header = () => {
 };
 
 export default Header;
-// import lovdlogo from '../assets/lovdlogo.svg';
-// import magnify from '../assets/magnify.svg';
-
-// const Header = () => {
-//   return (
-//     <header className="sticky top-0 z-50 w-full px-[160px] py-4 bg-white shadow-md">
-//       <div className="grid grid-cols-12 items-center space-x-4">
-//         {/* Logo and Title Div */}
-//         <div className="col-start-1 col-span-3 flex items-center space-x-3">
-//           <img
-//             src={lovdlogo}
-//             alt="L0vd Logo"
-//             className="w-[32px] h-[32px]"
-//           />
-//           <div className="flex flex-col">
-//             <span className="font-poppins font-semibold text-2xl tracking-wider">
-//               Berarank
-//             </span>
-//             <span className="font-poppins font-medium text-sm text-gray-500">
-//               powered by <img src={lovdlogo} alt="Powered by L0vd" className="inline-block w-4 h-4 ml-1" />L0vd
-//             </span>
-//           </div>
-//         </div>
-
-//         {/* Search Bar */}
-//         <div className="col-start-5 col-span-4 flex items-center justify-center">
-//           <div className="relative w-full max-w-md">
-//             <input
-//               type="text"
-//               placeholder="Search"
-//               className="w-full h-[45px] ring-1 ring-inset ring-lightblue-stroke font-poppins font-medium text-sm text-gray-600 rounded-full px-12 py-3"
-//             />
-//             <span className="absolute top-1/2 left-4 transform -translate-y-1/2 text-gray-400">
-//               <img src={magnify} alt="magnify" className="w-5 h-5" />
-//             </span>
-//           </div>
-//         </div>
-
-//         {/* Right Section: Validators, Pools, Guides, and Connect Button */}
-//         <div className="col-start-9 col-span-4 flex justify-end items-center space-x-8">
-//           <nav className="flex space-x-8">
-//             <a
-//               href="#"
-//               className="font-poppins font-medium text-base text-black hover:text-main-blue transition-colors"
-//             >
-//               Validators
-//             </a>
-//             <a
-//               href="#"
-//               className="font-poppins font-medium text-base text-black hover:text-main-blue transition-colors"
-//             >
-//               Pools
-//             </a>
-//             <a
-//               href="#"
-//               className="px-4 py-1.5 bg-main-blue text-white rounded-lg font-poppins font-medium text-base shadow-md hover:bg-blue-600 transition-colors"
-//             >
-//               Guides
-//             </a>
-//           </nav>
-//           <button className="px-6 py-2.5 bg-main-blue text-white rounded-lg font-poppins font-medium text-base shadow-md hover:bg-blue-600 transition-colors">
-//             Connect Wallet
-//           </button>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
--- src/components/stakecomponents.jsx/HoverCard.jsx
+++ src/components/stakecomponents.jsx/HoverCard.jsx
@@ -2,20 +2,33 @@ import React from 'react';
 
 function HoverCard() {
   return (
-    <div >
-      <h3 className="font-bold mb-2">L0vd score - find the best validator to stake</h3>
-      <p className="mb-2">
+    <div className='px-4'>
+      <h3 className="font-poppins font-bold text-[16px] text-black mb-2">L0vd score - find the best validator to stake</h3>
+      <p className="font-poppins font-regular text-[12px] mb-2">
         We track historical performance, including commission changes, APY history, and gauge updates,
         while also assessing the risk profile of each validator. Our scoring system rewards actions that 
         benefit the network and penalizes harmful behavior, ensuring you get the most reliable recommendations.
       </p>
-      <h4 className="font-semibold mb-1">How we calculate it</h4>
-      <p className="mb-2">
+      <h4 className="font-poppins font-bold text-[16px] mb-1">How we calculate it</h4>
+      <p className="font-poppins font-regular text-[12px] text-grey-stroke mb-2">
         Score version: v0.1
       </p>
-      <p className="mb-2">APY: 0-50%</p>
-      <p className="mb-2">Decentralization: 0-100%</p>
-      <p className="text-blue-500 underline">L0vd Score = 0.5 * APY Score + 0.5 * Decentralization Score</p>
+      <p className='ml-28 font-poppins font-bold text-[12px]'>Score</p>
+      <div className='flex flex-col font-poppins font-regular text-[12px]'>
+      <div className='flex flex-row space-x-20'>
+        
+
+      <p className="mb-2 mr-2">APY</p>
+      <p className="mb-2">0-100%</p>
+        </div>
+        <div className='flex flex-row space-x-3'>
+        
+
+      <p className="mb-2">Decentralization</p>
+      <p className="mb-2">0-100%</p>
+        </div>
+      </div>
+      <p className="font-poppins font-bold text-[12px] text-main-blue">L0vd Score = 0.5 * APY Score + 0.5 * Decentralization Score</p>
     </div>
   );
 }
